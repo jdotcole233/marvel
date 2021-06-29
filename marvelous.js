090597c265acba060c8a2af2cf09734b635825f0
jdotcole233 <jdotcole233@gmail.com>
2021-06-29 10:57:56 +0000

    methods testing with foreach

--- marvelous.js
+++ marvelous.js
@@ -9,15 +9,34 @@ const human = {
 
 
 // Array methods to note when programming 
-// Pop, Push
+// Pop, Push, forEach, map
 
 
 const cars = ["Audi", "BMW", "Toyota"];
-console.log("cars before ", cars);
+const carmodels = [
+  { id: 1, make: "Audi", model: "A11" },
+  { id: 2, make: "Bmw", model: "x6" },
+  { id: 3, make: "Ford", model: "Focus" },
+  { id: 4, make: "Range Rover", model: "Hse" },
+  { id: 5, make: "Rolls Royce", model: "Phantom" },
+];
+
+
+console.log(carmodels);
+const model = carmodels.map(car => car.model);
+console.log(model);
+
+
+// console.log("cars before ", cars);
+
+// const new_cars = cars.map(value => value.toUpperCase());
+
+// console.log(new_cars);
+// console.log(cars);
 
 // console.log(mutate(cars));
 
-console.log(mutateWithForEach(cars));
+// console.log(mutateWithForEach(cars));
 // cars.forEach((value, index) => {
 //      const msg = `i love ${value}`;
 //      cars[index] = msg.toLowerCase();
