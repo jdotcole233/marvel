1918f963aaa347c64616b757ead0195664f6b8d4
jdotcole233 <jdotcole233@gmail.com>
2021-06-29 11:36:17 +0000

    model extended

--- marvelous.js
+++ marvelous.js
@@ -9,34 +9,43 @@ const human = {
 
 
 // Array methods to note when programming 
-// Pop, Push, forEach, map
+// Pop, Push, forEach, map, filter
 
 
 const cars = ["Audi", "BMW", "Toyota"];
 const carmodels = [
   { id: 1, make: "Audi", model: "A11" },
   { id: 2, make: "Bmw", model: "x6" },
-  { id: 3, make: "Ford", model: "Focus" },
-  { id: 4, make: "Range Rover", model: "Hse" },
-  { id: 5, make: "Rolls Royce", model: "Phantom" },
+  { id: 3, make: "Bmw", model: "m6" },
+  { id: 4, make: "Bmw", model: "m3" },
+  { id: 5, make: "Ford", model: "Focus" },
+  { id: 6, make: "Range Rover", model: "Hse" },
+  { id: 7, make: "Range Rover", model: "Sports" },
+  { id: 8, make: "Rolls Royce", model: "Phantom" },
 ];
 
-const model_ = data => data.model;
-const make_ = data => data.make.toUpperCase();
 
-function name_make (data) {
-    return data.make.toLowerCase();
-}
 
-const anony_make = function (data) {
-    return data.id
-};
+
+
+// const model_ = data => data.model;
+// const make_ = data => data.make.toUpperCase();
+
+// function name_make (data) {
+//     return data.make.toLowerCase();
+// }
+
+// const anony_make = function (data) {
+//     return data.id
+// };
+
+// const make_model = car => `${car.make} => ${car.model}`;
 
 // const
 
 // console.log(carmodels);
-const model = carmodels.map(anony_make);
-console.log(model);
+// const model = carmodels.map(car => `${car.make} => ${car.model}`);
+// console.log(model);
 
 
 // console.log("cars before ", cars);
